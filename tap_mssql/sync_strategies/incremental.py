f15fd171ed8fbd5a0f0ef5ec0c27b9a7728e92a8
Roger Cheng <roger.cheng@emiratesline.com>
2024-04-18 12:02:49 +0800

    Revised for ESL

--- tap_mssql/sync_strategies/incremental.py
+++ tap_mssql/sync_strategies/incremental.py
@@ -54,13 +54,13 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns):
 
             if replication_key_value is not None:
                 if catalog_entry.schema.properties[replication_key_metadata].format == "date-time":
-                    replication_key_value = pendulum.parse(replication_key_value)
+                    replication_key_value = datetime.fromtimestamp(replication_key_value.timestamp())
 
                 select_sql += ' WHERE "{}" >= %(replication_key_value)s ORDER BY "{}" ASC'.format(
                     replication_key_metadata, replication_key_metadata
                 )
 
-                params["replication_key_value"] = datetime.fromtimestamp(replication_key_value.timestamp())
+                params["replication_key_value"] = replication_key_value
             elif replication_key_metadata is not None:
                 select_sql += ' ORDER BY "{}" ASC'.format(replication_key_metadata)
 
